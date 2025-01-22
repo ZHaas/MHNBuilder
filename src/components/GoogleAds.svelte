72db4e73e1d80ae1fed12914542c680fc06c77ec
Zach <zach.haas@withone.vision>
2025-01-22 12:56:35 -0500

    fix(ads): another attempt

---
+++ src/components/GoogleAds.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { isAdLoaded } from '$lib/stores';
+	$isAdLoaded = true; //sets the store to true implying Adblock is not enabled
+</script>
+
+<div>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7888254099507859"
+		crossorigin="anonymous"
+	></script>
+	<!-- webjeda-top-banner-ad -->
+	<ins
+		class="adsbygoogle"
+		style="display:block"
+		data-ad-client="ca-pub-7888254099507859"
+		data-ad-slot="1513412744"
+		data-ad-format="horizontal"
+		data-full-width-responsive="true"
+	></ins>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+	let Ads;
+	onMount(async () => {
+		Ads = (await import('../components/GoogleAds.svelte')).default;
+	});
 </script>
 
 <slot />
+<svelte:component this={Ads} />
